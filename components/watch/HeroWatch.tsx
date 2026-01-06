a8011d391725fde58ace38bc645161ca2c8b4ffa
Reiland, Cohen <cohen200521@gmail.com>
2026-01-06 16:44:03 -0600

    Added Light Mode Verion to Watch

--- components/watch/HeroWatch.tsx
+++ components/watch/HeroWatch.tsx
@@ -12,13 +12,13 @@ export default function HeroWatch({ imageSrc, onUp, onDown, }: HeroWatchProps) {
             {/* Top and Bottom Bands */}
             <div className="absolute top-0 left-1/2 -translate-x-1/2
                   w-[140px] h-[90px]
-                  bg-gradient-to-b from-gray-700 to-gray-900
+                  bg-gradient-to-b from-neutral-300 to-neutral-400 dark:from-gray-700 dark:to-gray-900
                   rounded-b-xl
                   shadow-md z-0"
             />
             <div className="absolute bottom-0 left-1/2 -translate-x-1/2
                   w-[140px] h-[90px]
-                  bg-gradient-to-t from-gray-700 to-gray-900
+                  bg-gradient-to-b from-neutral-300 to-neutral-400 dark:from-gray-700 dark:to-gray-900
                   rounded-t-xl
                   shadow-md z-0"
             />
@@ -30,8 +30,7 @@ export default function HeroWatch({ imageSrc, onUp, onDown, }: HeroWatchProps) {
                 aria-label="Scroll Up"
                 className="absolute left-[8px] top-[97px] rotate-[15deg]
                    w-7 h-14 rounded-md
-                   bg-gradient-to-b from-gray-600 to-gray-700
-                   border border-gray-500
+                   bg-gradient-to-b from-neutral-300 to-neutral-400 dark:from-gray-600 dark:to-gray-700 border border-neutral-400 dark:border-gray-500
                    shadow-lg
                    active:translate-x-[2px] active:translate-y-[1px]"
             />
@@ -43,31 +42,29 @@ export default function HeroWatch({ imageSrc, onUp, onDown, }: HeroWatchProps) {
                 aria-label="Scroll Down"
                 className="absolute left-[10px] top-[200px] rotate-[-20deg]
                    w-7 h-14 rounded-md
-                   bg-gradient-to-b from-gray-600 to-gray-700
-                   border border-gray-500
+                   bg-gradient-to-b from-neutral-300 to-neutral-400 dark:from-gray-600 dark:to-gray-700 border border-neutral-400 dark:border-gray-500
                    shadow-lg
                    active:translate-x-[2px] active:translate-y-[-1px]"
             />
             {/* Watch Case */}
             <div className="relative w-[300px] h-[300px] rounded-full
-                            bg-gradient-to-br from-gray-700 to-gray-900
-                            shadow-[0_30px_60px_rgba(0,0,0,0.7)]">
+                                bg-gradient-to-br from-neutral-400 to-neutral-300 dark:from-gray-700 dark:to-gray-900
+                            shadow-[0_30px_60px_rgba(0,0,0,0.6)]">
                 {/* Watch Bezel */}
                 <div className="absolute inset-4 rounded-full
-                                bg-black ring-1 ring-white/20" >
+                                bg-neutral-300 dark:bg-black ring-1 ring-black/10 dark:ring-white/20" >
                     {/* Up & Down Markers */}
                     <div className="absolute left-2 top-0 bottom-0 flex flex-col justify-center gap-20 pointer-events-none">
-                        <span className="text-[11px] tracking-widest text-gray-300 rotate-[-75deg]">
+                        <span className="text-[11px] tracking-widest text-neutral-600 dark:text-gray-300 rotate-[-75deg]">
                             UP
                         </span>
-                        <span className="text-[11px] tracking-widest text-gray-300 rotate-[65deg]">
+                        <span className="text-[11px] tracking-widest text-neutral-600 dark:text-gray-300 rotate-[65deg]">
                             DOWN
                         </span>
                     </div>
 
                     {/* Watch Screen */}
-                    <div className="absolute inset-8 rounded-full overflow-hidden bg-gray-900">
-
+                    <div className="absolute inset-8 rounded-full overflow-hidden bg-neutral-100 dark:bg-gray-900">
                         <img
                             src={imageSrc}
                             alt="Profile Photo"
