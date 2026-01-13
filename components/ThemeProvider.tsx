aed7001e3a2586ab9bd07f20fab71d85b1309373
Reiland, Cohen <cohen200521@gmail.com>
2026-01-13 17:56:20 -0600

    Changed Default Mode to Dark

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -13,7 +13,7 @@ const ThemeContext = createContext<{
 })
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
-    const [theme, setTheme] = useState<Theme>("light")
+    const [theme, setTheme] = useState<Theme>("dark")
 
     useEffect(() => {
         const savedTheme = localStorage.getItem("theme") as Theme
